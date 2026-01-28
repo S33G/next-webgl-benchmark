d02745b13af0e738601097b73d07beffde0057e7
S33g <dev@charlie.fyi>
2026-01-28 02:54:53 +0000

    ci: github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: 'out',
+  basePath: process.env.BASE_PATH || '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
